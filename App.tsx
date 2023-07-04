ff664a1a0e38bf3105eb05ab567d031191bf6447
lotusCupcake <fikriansari.mfa@gmail.com>
2023-07-04 15:21:01 +0700

    class component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import {Image, Text, TextInput, View} from "react-native"
 
 const App = () => {
@@ -8,7 +8,9 @@ const App = () => {
       <Text>Fikri</Text>
       <Ansari/>
       <Photo />
-      <TextInput style={{marginTop:15,borderWidth: 1}}/>
+      <TextInput style={{ marginTop: 15, borderWidth: 1 }} />
+      <BoxGreen/>
+      <Profile />
     </View>
   )
 }
@@ -22,6 +24,25 @@ const Photo = () => {
     source={{ uri: 'https://loremflickr.com/640/360' }}
     style={{ width: 100, height: 100 }}
   />
+}
+
+class BoxGreen extends Component{
+  render() {
+    return <Text>Ini merupakan contoh komponen menggunakan class</Text>
+  }
+}
+
+class Profile extends Component{
+  render() {
+    return (
+      <View style={{alignItems:"center",marginTop: 15}}>
+        <Image
+          source={{ uri: 'https://placekitten.com/640/360' }}
+          style={{ width: 100, height: 100,borderRadius:50 }}
+        />
+        <Text style={{ color:'blue',fontSize:20 }}>Kucing lucu</Text>
+      </View>)
+  }
 }
 
 export default App
