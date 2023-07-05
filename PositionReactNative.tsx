257070467245f4f10ee6943ad1bebee96eb8fe9d
lotusCupcake <fikriansari.mfa@gmail.com>
2023-07-05 16:54:20 +0700

    position

--- PositionReactNative.tsx
+++ PositionReactNative.tsx
@@ -8,7 +8,7 @@ return(
     <View style={{ alignItems: 'center' }}>
         <View style={{ borderWidth:2, borderColor:'black',width:150, height:150, borderRadius:120,justifyContent:'center',alignItems:'center'}}>
             <Cart />
-            <Text style={{fontSize:20, color:'black', backgroundColor:'green',color:'white', padding:5, borderRadius:25, position:"absolute" ,top:0, right:0}}>10</Text>
+            <Text style={{fontSize:20, backgroundColor:'green',color:'white', padding:5, borderRadius:25, position:"absolute" ,top:0, right:0}}>10</Text>
         </View> 
         <Text style={{fontSize:20, color:'black', marginTop:10}}>Keranjang Belanja Anda</Text>
   </View>)
