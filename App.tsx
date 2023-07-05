b7ae1e1e1fbf27da47cec1e8c08c5ea6537bc364
lotusCupcake <fikriansari.mfa@gmail.com>
2023-07-05 10:46:21 +0700

    basic styling

--- App.tsx
+++ App.tsx
@@ -1,7 +1,38 @@
 import React, { Component } from "react"
-import {Image, Text, TextInput, View} from "react-native"
+import { Image, StyleSheet, Text, TextInput, View } from "react-native"
+import cat from './cat.jpg'
 
-const App = () => {
+function App() {
+  return <StylingReactNativeComponent />
+}
+
+const StylingReactNativeComponent = () => {
+  return(
+  <View>
+      <View style={{padding:12, backgroundColor:'#F2F2F2', width:212, borderRadius:8}}>
+        <Image source={cat} style={{ width: 188, height: 107, borderRadius: 8 }} />
+        <Text style={{fontSize:14,fontWeight:'bold',color:'black',marginTop:16}}>New Cat Available</Text>
+        <Text style={{fontSize:12,fontWeight:'bold',color:'#F2994A',marginTop:16}}>Rp. 500.000</Text>
+        <Text style={{fontSize:12,fontWeight:'bold',color:'black',marginTop:12}}>Medan</Text>
+        <View style={{backgroundColor:'#6FCF97',paddingVertical:6,borderRadius:25,marginTop:20}}>
+          <Text style={{fontSize:14,fontWeight:'400',color:'white','textAlign':'center'}}>BELI</Text>
+      </View>
+      </View>
+  </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "green",
+    marginLeft: 20,
+    marginTop: 20,
+  }
+})
+
+const SampleComponent = () => {
   return (
     <View>
       <View style={{width:80, height:80,backgroundColor:'red'}} />
